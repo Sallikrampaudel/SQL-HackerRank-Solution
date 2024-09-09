1293b8adec6b699632731e1710291e9517272360
SalikPaudel <36765526+Sallikrampaudel@users.noreply.github.com>
2024-09-08 20:05:22 -0500

    Update and rename Select by ID to Select by ID.sql

--- Select by ID.sql
+++ Select by ID.sql
@@ -1,3 +1,4 @@
+"""
 ###Select by ID
 
 Query all columns for a city in CITY with the ID 1661.
@@ -12,6 +13,7 @@ NAME	VARCHAR2(17)
 COUNTRY CODE	VARCHAR2(3)
 DISTRICT	VARCHAR2(20)
 POPULATION	NUMBER
-Solution
+"""
+###Solution
 
 SELECT * FROM CITY WHERE ID = 1661; 
